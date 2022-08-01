00418f0208f92cf24d078e375ffff8b28311933b
Jose Rivas <jirivasmo@gmail.com>
2022-08-01 16:20:18 -0500

    test sonarcloud

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, Jhon/p>"
+
+def HELLOWorlds():
+    pass
+
+def GoodBye_world():
+    pass
+def godbye_world():
+    pass
